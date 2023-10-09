626b9688861b5b4e7e3884c59d9ca0ff2891ff22
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-09 20:20:42 +0600

    adding comments for better understanding the codes

--- DOM-P03/JS/phone-2.js
+++ DOM-P03/JS/phone-2.js
@@ -1,3 +1,4 @@
+// Function to update the input field value
 function updateInputNumbers(isIncrease) {
     const numberInputField = document.getElementById("case-input-field");
     const numberInputString = numberInputField.value;
@@ -6,24 +7,27 @@ function updateInputNumbers(isIncrease) {
     if (isIncrease) {
         newNumberInput = previousNumberInput + 1;
     } else {
+        // Ensure the value doesn't go below 0
         newNumberInput = Math.max(previousNumberInput - 1, 0);
     }
     numberInputField.value = newNumberInput;
     return newNumberInput;
 }
 
-function updateInputTotalPrice (newNumberInput){
-    const numberTotalPrice = newNumberInput * 39;
-    const numberTotalElement = document.getElementById ("case-price-total");
+// Function to update the total price based on the input value
+function updateInputTotalPrice(newNumberInput) {
+    const numberTotalPrice = newNumberInput * 39; // Assuming the price per item is $39
+    const numberTotalElement = document.getElementById("case-price-total");
     numberTotalElement.innerText = numberTotalPrice;
 }
+
+// Add an event listener to the "case-btn-plus" button
 document.getElementById("case-btn-plus").addEventListener("click", function () {
     const newNumberInput = updateInputNumbers(true);
     updateInputTotalPrice(newNumberInput);
-
 });
 
-// Remove the second event listener, as it's a duplicate
+// Add an event listener to the "case-btn-minus" button
 document.getElementById("case-btn-minus").addEventListener("click", function () {
     const newNumberInput = updateInputNumbers(false);
     updateInputTotalPrice(newNumberInput);
