6cca8f0d2de664ba6874b3d0b2b7d6f636e9e58d
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-09 18:10:12 +0600

    using function

--- DOM-P03/JS/case.js
+++ DOM-P03/JS/case.js
@@ -1,5 +1,5 @@
-// Get the button element with the id "btn-case-plus"
-document.getElementById("btn-case-plus").addEventListener("click", function() {
+// Function to update the input number based on whether toIncrease is true or false
+function updateInputNumber(toIncrease) {
     // Get the input field element with the id "case-input-field"
     const caseInputField = document.getElementById("case-input-field");
 
@@ -9,9 +9,28 @@ document.getElementById("btn-case-plus").addEventListener("click", function() {
     // Convert the string to an integer (assuming it contains a valid number)
     const previousCaseInput = parseInt(caseInputString);
 
-    // Increment the previous input value by 1 to get the new value
-    const newCaseInput = previousCaseInput + 1;
+    let newCaseInput;
+
+    // Check if toIncrease is true or false to determine whether to increment or decrement
+    if (toIncrease === true) {
+        newCaseInput = previousCaseInput + 1;
+    } else {
+        // Prevent the value from going below 0
+        newCaseInput = Math.max(previousCaseInput - 1, 0);
+    }
 
     // Update the input field with the new value
     caseInputField.value = newCaseInput;
+}
+
+// Get the button element with the id "btn-plus"
+document.getElementById("btn-plus").addEventListener("click", function() {
+    // Call the updateInputNumber function with true to increase the input value
+    updateInputNumber(true);
+});
+
+// Get the button element with the id "btn-minus"
+document.getElementById("btn-minus").addEventListener("click", function() {
+    // Call the updateInputNumber function with false to decrease the input value
+    updateInputNumber(false);
 });
