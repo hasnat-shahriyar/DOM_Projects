5379854125988e74dfc92a4734e406ee938f279b
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-09 20:18:36 +0600

    adding function to the second option for plus and minus button with price changing feature

--- DOM-P03/JS/phone-2.js
+++ DOM-P03/JS/phone-2.js
@@ -1,7 +1,30 @@
-document.getElementById("btn-plus2").addEventListener ("click", function(){
-    const numberInputField = document.getElementById ("input-field2");
+function updateInputNumbers(isIncrease) {
+    const numberInputField = document.getElementById("case-input-field");
     const numberInputString = numberInputField.value;
-    const previousNumberInput = parseInt (numberInputString);
-    const newNumberInput = previousNumberInput + 1;
+    const previousNumberInput = parseInt(numberInputString);
+    let newNumberInput;
+    if (isIncrease) {
+        newNumberInput = previousNumberInput + 1;
+    } else {
+        newNumberInput = Math.max(previousNumberInput - 1, 0);
+    }
     numberInputField.value = newNumberInput;
-})
+    return newNumberInput;
+}
+
+function updateInputTotalPrice (newNumberInput){
+    const numberTotalPrice = newNumberInput * 39;
+    const numberTotalElement = document.getElementById ("case-price-total");
+    numberTotalElement.innerText = numberTotalPrice;
+}
+document.getElementById("case-btn-plus").addEventListener("click", function () {
+    const newNumberInput = updateInputNumbers(true);
+    updateInputTotalPrice(newNumberInput);
+
+});
+
+// Remove the second event listener, as it's a duplicate
+document.getElementById("case-btn-minus").addEventListener("click", function () {
+    const newNumberInput = updateInputNumbers(false);
+    updateInputTotalPrice(newNumberInput);
+});
